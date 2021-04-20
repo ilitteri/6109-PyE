599d3d3350fba4c961a6fdf5b8d1af18c87a94e7
Ivan <ilitteri@fi.uba.ar>
2021-04-20 07:24:02 -0300

    Preparacion de animacion de calculo de probabilidades con dado

--- animaciones/dice.py
+++ animaciones/dice.py
@@ -1,20 +1,7 @@
 from manim import *
 
-DICE_FACES = [
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six"
-]
-
 class Foo(Scene):
     def construct(self):
-        # faces = VGroup(SVGMobject(f'dice-six-faces-{face}.svg' for face in DICE_FACES))
-        # for face in faces:
-        #     self.play(Create(face))
-        #     self.wait()
-        one = SVGMobject("/home/ivan/LaTeX/6109-PyE/animaciones/dice-six-faces-five.svg")
-        self.play(Write(one))
+        s = SampleSpace()
+        self.play(Create(s))
         self.wait()
